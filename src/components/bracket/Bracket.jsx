3aa43bca36a458cfa3bf0861f432a5a9fc67458b
Joe Malatesta <joemalatesta@msn.com>
2024-01-21 11:45:58 -0500

    refactoring code.

--- src/components/bracket/Bracket.jsx
+++ src/components/bracket/Bracket.jsx
@@ -1,8 +1,9 @@
 import SingleMatch from './SingleMatch';
-
-const Bracket = ({round}) => {
-
- console.log(round)
+import * as gameServices from '../../services/gameServices'
+const Bracket = (props) => {
+  console.log(props.gameObj);
+  let round = gameServices.SplitIntoMatches(props.gameObj.matchPlayers)
+  console.log(round);
   return (
     <>
       {round?.map((match, idx) => (
--- src/components/bracket/BracketLayout.jsx
+++ src/components/bracket/BracketLayout.jsx
@@ -13,15 +13,11 @@ const BracketLayout = ({gameObj, user}) => {
           <div className="flex-column"id='match-1'>
             {gameObj.rounds.map((round, idx) =>(
               <Bracket 
-                round={round}
                 key={idx}
                 user={user}
                 gameObj={gameObj}
               />
-
-            )
-
-            )}
+            ))}
           </div>
         </div>
       </div>
@@ -31,3 +27,17 @@ const BracketLayout = ({gameObj, user}) => {
 
 export default BracketLayout;
 
+
+
+
+/*
+need to have array's of player IDs
+the main one will be the matchplayers array
+there will be an array of arrays, this outer array will hold arrays of each round of match players
+this will be passed from braketlayout to bracket
+in bracket we will brake up the array (rounds) into 2 person matches.
+this is where we will add bye players if needed
+this bracket will hold a single match comp. this will hold a pair of players
+the single match will hold a singlematchplayer comp. this is where we will get the info for the player and apply the checkboxes (also have a req to pull all new data from the db refresh hack)
+
+*/
