b17a05a8ed4d2f190533eff31841e2f15329fa28
Joe Malatesta <joemalatesta@msn.com>
2024-03-03 16:52:26 -0500

    read the frickin errors LMAO

--- src/pages/CreateSchedule/CreateSchedule.jsx
+++ src/pages/CreateSchedule/CreateSchedule.jsx
@@ -6,36 +6,36 @@ const CreateSchedule = (props) => {
   const [enteredDate, setEnteredDate] = useState('')
   const [homeTeam, setHomeTeam] = useState(null)
   const [visitor, setVisitor] = useState(null)
-  const [table, setTable] = useState(null)
+  const [table, setTable] = useState([1,2,3,4])
   const [matches, setMatches] = useState([])
-  const [usedTeams, setUsedTeams] = useState([])
+  const [adjustedTeams, setAdjustedTeams] = useState(props.teams)
+  const [usedTable, setUsedTable] = useState()
 
-  // const [formData, setFormData] = useState({
-  //   homeTeam: { homeTeam },
-  //   visitor: { visitor },
-  //   table: { table },
-  // })
 
   const handleChangeDate = (event) => {
     setEnteredDate(event.target.value)
   }
 
   const handleChangeTable = (evt) => {
-    setTable(evt.target.value)
+    setUsedTable(evt.target.value)
   }
 
   const handleSelectTeam = (team) => {
     if (homeTeam === null) setHomeTeam(team)
     if (visitor === null && homeTeam !== null) setVisitor(team)
+    setAdjustedTeams(adjustedTeams.filter((el) => el._id !== team._id))
   }
 
   const handleSubmitMatch = () => {
     if(matches.length < 5){
-      setMatches([...matches, {homeTeam, visitor, table}])
+      console.log(usedTable);
+      setMatches([...matches, {homeTeam, visitor, usedTable}])
+      setTable(table.filter((el) => el !== parseInt(usedTable))
+      )
     }
     setHomeTeam(null)
     setVisitor(null)
-    setTable(null)
+    setUsedTable(null)
   }
 
   console.log(matches);
@@ -58,36 +58,35 @@ const CreateSchedule = (props) => {
       </h2>
       <h2>Home - {homeTeam === null ? "Pick a team" : homeTeam?.teamName}</h2>
       <h2>Visitors - {visitor === null ? "Pick a team" : visitor?.teamName}</h2>
-      <h2>table: {table}</h2>
+      <h2>table: {usedTable}</h2>
 
       <button onClick={()=> handleSubmitMatch()}>submit match</button>
 
-      <h2>
-        Pick the {visitor === null && homeTeam !== null ? "Visitor" : "Home"}{" "}
-        Team:
-      </h2>
-      {props?.teams?.map((team) => (
-        <ul style={{ color: "white" }} key={team._id}>
-          <li onClick={() => handleSelectTeam(team)}>{team.teamName}</li>
-        </ul>
-      ))}
       <div
         type="text"
         id="table"
         name="table"
-        value={table}
+        value={usedTable}
         onChange={handleChangeTable}
         required
       >
         Choose the table -
         <select name="table" id="table">
           <option value="">pick a table</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
+          {table.map(num => (
+            <option key={num} value={num}>{num}</option> 
+          ))}
         </select>
       </div>
+      <h2>
+        Pick the {visitor === null && homeTeam !== null ? "Visitor" : "Home"}{" "}
+        Team:
+      </h2>
+      {adjustedTeams.map((team) => (
+        <ul style={{ color: "white" }} key={team._id}>
+          <li onClick={() => handleSelectTeam(team)}>{team.teamName}</li>
+        </ul>
+      ))}
       <p>
         need to have the rank of the player(at the time the match is being
         played)
