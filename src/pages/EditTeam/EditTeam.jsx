4158ed7d72039250922c02602afafdbecf373fc0
Joe Malatesta <joemalatesta@msn.com>
2024-06-12 11:16:43 -0400

    add a sorted version so winning percentage is at the top

--- src/pages/EditTeam/EditTeam.jsx
+++ src/pages/EditTeam/EditTeam.jsx
@@ -1,10 +1,18 @@
 const EditTeam = (props) => {
-  // for future plans
+  const calculateWinPercentage = (team) => {
+    const totalMatches = team.wins + team.loss
+    return totalMatches === 0 ? 0 : (team.wins / totalMatches) * 100
+  }
+
+  const sortedTeams = [...props.teams].sort(
+    (a, b) => calculateWinPercentage(b) - calculateWinPercentage(a)
+  )
+
   console.log(props)
   return (
     <>
       <h1>this is the teams edit page</h1>
-      <h2>{props?.teams?.map(el =>
+      <h2>{sortedTeams?.map(el =>
         <div key={el._id} className="bracket" style={{width: 350}}>
           {el.teamName}<br/> Wins:{el.wins}<br/>  Losses: {el.loss}<br/> Win %: {isNaN(((el.wins / (el.wins + el.loss)) * 100).toFixed(2)) ? "0.00" : ((el.wins / (el.wins + el.loss)) * 100).toFixed(2)} 
         </div>
