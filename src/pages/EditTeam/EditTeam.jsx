fddadac3a06d39a030a9d90e403c883796823e7a
Joe Malatesta <joemalatesta@msn.com>
2024-06-12 11:13:06 -0400

    add win%"

--- src/pages/EditTeam/EditTeam.jsx
+++ src/pages/EditTeam/EditTeam.jsx
@@ -5,9 +5,9 @@ const EditTeam = (props) => {
     <>
       <h1>this is the teams edit page</h1>
       <h2>{props?.teams?.map(el =>
-        <li key={el._id}>
-          {el.teamName} Wins:{el.wins} losses: {el.loss}
-        </li>
+        <div key={el._id} className="bracket" style={{width: 350}}>
+          {el.teamName}<br/> Wins:{el.wins}<br/>  Losses: {el.loss}<br/> Win %: {isNaN(((el.wins / (el.wins + el.loss)) * 100).toFixed(2)) ? "0.00" : ((el.wins / (el.wins + el.loss)) * 100).toFixed(2)} 
+        </div>
       )}</h2>
     </>
   )
